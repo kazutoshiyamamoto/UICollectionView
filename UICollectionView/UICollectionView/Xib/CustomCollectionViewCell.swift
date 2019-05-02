9f62401356fb9e06447a1dd204e2b094aab6d8dc
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-05-02 14:51:53 +0900

    カスタムセルの追加

---
+++ UICollectionView/UICollectionView/Xib/CustomCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCollectionViewCell.swift
+//  UICollectionView
+//
+//  Created by home on 2019/05/02.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
